af94144d08ca62f8a66d645c64a45c59951766bc
Wesley <83217085+Wesbreezy@users.noreply.github.com>
2025-04-01 23:35:45 +0200

    Delete util directory

--- util/Identifier.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.kotlin.course.alias.util
-
-typealias Identifier = Int
-
-class IdentifierFactory {
-    var counter: Int = 0
-    fun uniqueIdentifier(): Identifier = counter++
-}
