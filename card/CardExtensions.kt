a7a4f2cbda982a1e4e2cadb93e48bdaf86502352
Wesley <83217085+Wesbreezy@users.noreply.github.com>
2025-04-01 23:35:19 +0200

    Delete card directory

--- card/CardExtensions.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.kotlin.course.alias.card
-
-fun Card.toJsCard(): JsCard {
-    return JsCard(
-        id = this.id,
-        words = this.words.map { it.word }.toTypedArray()
-    )
-}
